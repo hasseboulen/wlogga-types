bbc07d18a9263b3d94bf32d0a642a53ee6364f0c
Hans Ripa <hasseboulen@gmail.com>
2020-06-03 15:46:48 +0200

    Update.

--- typings/models/Category.ts
+++ typings/models/Category.ts
@@ -9,100 +9,90 @@ import { IJobModelGraphql } from "./Job";
 
 import { JobColumn, JobQueryColumn } from "../JobColumn";
 
-import {
-  SummerizationModules,
-  SummerizationTimes,
-  JobPermissionLevel,
-} from "../enums";
+import { SummerizationModules, SummerizationTimes, JobPermissionLevel } from "../enums";
 
 export type ViewStateColumns = { [x in JobColumn]: boolean } & {
-  __typename: string;
+	__typename: string;
 };
 export type ViewStateJobQuery = { [x in JobQueryColumn]: boolean | null } & {
-  __typename: string;
-  from: string;
+	__typename: string;
+	from: string;
 };
 
 export interface ICategoryViewState {
-  __typename: string;
-  _id: string;
-  sorted: { __typename: string; id: string; desc: boolean }[];
-  filtered: { id: string; value: string }[];
-  resized: { id: string; value: number }[];
-  expanded: Array<any>;
-  page: number;
-  pageSize: number;
+	__typename: string;
+	_id: string;
+	sorted: { __typename: string; id: string; desc: boolean }[];
+	filtered: { id: string; value: string }[];
+	resized: { id: string; value: number }[];
+	expanded: Array<any>;
+	page: number;
+	pageSize: number;
 
-  columns: ViewStateColumns;
-  jobQuery: ViewStateJobQuery;
+	columns: ViewStateColumns;
+	jobQuery: ViewStateJobQuery;
 
-  selectedJobIds: string[];
-  filteredJobIds: string[];
-  editJobIds: string[];
-  selectAll: boolean;
-  menuOpen: boolean;
-  printTemplateId: string | null;
+	selectedJobIds: string[];
+	filteredJobIds: string[];
+	editJobIds: string[];
+	selectAll: boolean;
+	menuOpen: boolean;
+	printTemplateId: string | null;
 
-  invoiceSummerizationModule: SummerizationModules;
-  invoiceSummerizationTime: SummerizationTimes;
+	invoiceSummerizationModule: SummerizationModules;
+	invoiceSummerizationTime: SummerizationTimes;
 
-  actions: {
-    __typename: string;
-    hasNonExamined: boolean;
+	actions: {
+		__typename: string;
+		hasNonExamined: boolean;
 
-    hasNonBilled: boolean;
-    hasBilled: boolean;
+		hasNonBilled: boolean;
+		hasBilled: boolean;
 
-    hasNonPaid: boolean;
-    hasPaid: boolean;
+		hasNonPaid: boolean;
+		hasPaid: boolean;
 
-    hasNonArchived: boolean;
-    hasArchived: boolean;
+		hasNonArchived: boolean;
+		hasArchived: boolean;
 
-    hasInvalidated: boolean;
-    hasValidated: boolean;
-  };
+		hasInvalidated: boolean;
+		hasValidated: boolean;
+	};
 }
 
-export interface ICategory
-  extends Id,
-    Position,
-    Active,
-    Timestamps,
-    GroupId,
-    Document {
-  oid: string;
-  oldId: string;
-  name: string;
-  description: string;
-  mainFieldId: MongoId;
-  billByFieldId: MongoId;
-  payByFieldId: MongoId;
-  groupJobsByDays: number;
-  jobCount: number;
-  medians: any;
-  maintenance: boolean;
-  maintenanceInterval: number;
-  maintenanceWeekends: boolean;
+export interface ICategory extends Id, Position, Active, Timestamps, GroupId, Document {
+	oid: string;
+	oldId: string;
+	name: string;
+	description: string;
+	mainFieldId: MongoId;
+	billByFieldId: MongoId;
+	payByFieldId: MongoId;
+	groupJobsByDays: number;
+	jobCount: number;
+	medians: { optionId: MongoId; value: any }[];
+	maintenance: boolean;
+	maintenanceInterval: number;
+	maintenanceWeekends: boolean;
 }
 
 export interface ICategoryModel extends ICategory {}
 
 export interface ICategoryModelGraphql extends ICategoryModel {
-  _edit: boolean;
-  _remove: boolean;
+	_edit: boolean;
+	_remove: boolean;
 
-  fields: IFieldModelGraphql[];
-  mainField: IFieldModelGraphql | null;
-  billField: IFieldModelGraphql | null;
-  payField: IFieldModelGraphql | null;
+	fields: IFieldModelGraphql[];
+	mainField: IFieldModelGraphql | null;
+	billField: IFieldModelGraphql | null;
+	payField: IFieldModelGraphql | null;
 
-  jobPermissions: JobPermissionLevel;
+	jobPermissions: JobPermissionLevel;
 
-  machines: IMachineModelGraphql[];
-  customers: ICustomerModelGraphql[];
+	machines: IMachineModelGraphql[];
+	customers: ICustomerModelGraphql[];
 
-  printTemplates: IPrintTemplateModelGraphql[];
-  jobs: IJobModelGraphql[];
-  viewState: ICategoryViewState;
+	printTemplates: IPrintTemplateModelGraphql[];
+	jobs: IJobModelGraphql[];
+	viewState: ICategoryViewState;
 }
--- typings/models/Invoice.ts
+++ typings/models/Invoice.ts
@@ -2,65 +2,59 @@ import { Document } from "mongoose";
 import { Id, Active, GroupId, CustomerId } from "./shared";
 import { MongoId } from "../MongoId";
 import { ICustomerModelGraphql } from "./Customer";
-import {
-  FieldUIElement,
-  SummerizationTimes,
-  SummerizationModules,
-} from "../enums";
+import { FieldUIElement, SummerizationTimes, SummerizationModules } from "../enums";
 import { IUserModelGraphql } from "./User";
 import { IJobModelGraphql } from "./Job";
 
 export enum InvoiceType {
-  BILL = "BILL",
-  PAY = "PAY",
+	BILL = "BILL",
+	PAY = "PAY",
 }
 
 export type IInvoiceData = {
-  categoryId: MongoId;
-  categoryName: string;
-  fields: {
-    fieldId: MongoId;
-    name: string;
-    uiElement: FieldUIElement;
-  }[];
-  data: IInvoicePDFRow;
-}[];
+	categoryId: MongoId;
+	categoryName: string;
+	fields: {
+		fieldId: MongoId;
+		name: string;
+		uiElement: FieldUIElement;
+	}[];
+	data: [IInvoicePDFRow[]];
+};
 
-export type IInvoicePDFRow = Array<
-  Array<{
-    performedBy: string;
-    performedAt: string;
-    [key: string]: string | null;
-  }>
->;
+export type IInvoicePDFRow = {
+	performedBy: string;
+	performedAt: string;
+	[key: string]: string | null;
+};
 
 export interface IInvoice extends Id, CustomerId, GroupId, Active, Document {
-  oid: string;
-  module: SummerizationModules;
-  moduleName: string;
-  summerization: SummerizationTimes;
-  groupName: string;
-  logo: any;
-  createdAt: Date;
-  createdByName: string;
-  createdBy: string;
-  preview: boolean;
-  withPerformedBy: boolean;
-  withDate: boolean;
-  data: IInvoiceData;
-  jobIds: MongoId[];
-  archived: boolean;
-  accessToken: string;
-  type: InvoiceType;
-  userId: MongoId;
+	oid: string;
+	module: SummerizationModules;
+	moduleName: string;
+	summerization: SummerizationTimes;
+	groupName: string;
+	logo: any;
+	createdAt: Date;
+	createdByName: string;
+	createdBy: string;
+	preview: boolean;
+	withPerformedBy: boolean;
+	withDate: boolean;
+	data: IInvoiceData[];
+	jobIds: MongoId[];
+	archived: boolean;
+	accessToken: string;
+	type: InvoiceType;
+	userId: MongoId;
 
-  paid: boolean;
-  billed: boolean;
+	paid: boolean;
+	billed: boolean;
 }
 
 export interface IInvoiceModel extends IInvoice {}
 export interface IInvoiceModelGraphql extends IInvoiceModel {
-  customer: ICustomerModelGraphql | null;
-  user: IUserModelGraphql | null;
-  jobs: IJobModelGraphql[];
+	customer: ICustomerModelGraphql | null;
+	user: IUserModelGraphql | null;
+	jobs: IJobModelGraphql[];
 }
