b58c6707e3448cb09ade1f2f0b1c7efe252e4baa
Hans Ripa <hasseboulen@gmail.com>
2020-06-04 11:41:02 +0200

    update.

--- typings/models/Job.ts
+++ typings/models/Job.ts
@@ -8,67 +8,61 @@ import { IMachineModelGraphql } from "./Machine";
 import { IUserModelGraphql } from "./User";
 
 export interface IJobFieldValue extends Id {
-  fieldId: MongoId;
-  label?: string;
-  value?: string;
-  number?: number;
-  duration?: null | number;
-  moment?: Date | Moment;
-  start?: null | Date | Moment;
-  end?: null | Date | Moment;
-  optionId?: MongoId;
-  machineId?: MongoId;
-  machine?: IMachineModelGraphql;
-  // JobId: string;
+	fieldId: MongoId;
+	label?: string;
+	value?: string;
+	number?: number;
+	duration?: null | number;
+	moment?: Moment | null | undefined;
+	start?: Moment | null | undefined;
+	end?: Moment | null | undefined;
+	optionId?: MongoId;
+	machineId?: MongoId;
+	machine?: IMachineModelGraphql;
+	// JobId: string;
 }
 
-export interface IJob
-  extends Id,
-    CategoryId,
-    CustomerId,
-    GroupId,
-    Timestamps,
-    Document {
-  RID: number;
-  fieldValues: IJobFieldValue[];
-  date: Date;
+export interface IJob extends Id, CategoryId, CustomerId, GroupId, Timestamps, Document {
+	RID: number;
+	fieldValues: IJobFieldValue[];
+	date: Date;
 
-  performed: boolean;
-  performedAt: Date;
-  performedBy: MongoId;
+	performed: boolean;
+	performedAt: Date;
+	performedBy: MongoId;
 
-  examined: boolean;
-  examinedAt: Date;
-  examinedBy: MongoId;
+	examined: boolean;
+	examinedAt: Date;
+	examinedBy: MongoId;
 
-  billed: boolean;
-  billedAt: Date;
-  billedBy: MongoId;
+	billed: boolean;
+	billedAt: Date;
+	billedBy: MongoId;
 
-  paid: boolean;
-  paidAt: Date;
-  paidBy: MongoId;
+	paid: boolean;
+	paidAt: Date;
+	paidBy: MongoId;
 
-  validated: boolean;
-  validatedAt: Date;
-  validatedBy: MongoId;
+	validated: boolean;
+	validatedAt: Date;
+	validatedBy: MongoId;
 
-  archived: boolean;
-  archivedAt: Date;
-  archivedBy: MongoId;
+	archived: boolean;
+	archivedAt: Date;
+	archivedBy: MongoId;
 }
 
 export interface IJobModel extends IJob {}
 export interface IJobForm extends Partial<IJobModel> {
-  date: Date;
-  fieldValues: IJobFieldValue[];
+	date: Date;
+	fieldValues: IJobFieldValue[];
 }
 
 export interface IJobModelGraphql extends IJobModel {
-  oid: string;
-  category: ICategoryModelGraphql | null;
-  customer: ICustomerModelGraphql | null;
-  creator: IUserModelGraphql | null;
-  performer: IUserModelGraphql | null;
-  [key: string]: any;
+	oid: string;
+	category: ICategoryModelGraphql | null;
+	customer: ICustomerModelGraphql | null;
+	creator: IUserModelGraphql | null;
+	performer: IUserModelGraphql | null;
+	[key: string]: any;
 }
