cd719ea866e5155dacfd71174daf7dad9a49af86
Hans Ripa <hasseboulen@gmail.com>
2020-06-17 14:42:51 +0200

    added availablecustomers to optiongroups.

--- typings/models/FieldOptionGroup.ts
+++ typings/models/FieldOptionGroup.ts
@@ -1,7 +1,7 @@
 import { Document } from "mongoose";
 import { Id, Active, GroupId } from "./shared";
 import { MongoId } from "../MongoId";
-import { IFieldModelGraphql } from "..";
+import { IFieldModelGraphql, ICustomerModelGraphql } from "..";
 import { IFieldOptionModelGraphql } from "./FieldOption";
 
 export interface IFieldOptionGroup extends Id, Document, Active, GroupId {
@@ -13,4 +13,5 @@ export interface IFieldOptionGroupModel extends IFieldOptionGroup {}
 export interface IFieldOptionGroupModelGraphql extends IFieldOptionGroupModel {
 	fields: IFieldModelGraphql[];
 	options: IFieldOptionModelGraphql[];
+	availableCustomers: ICustomerModelGraphql[];
 }
